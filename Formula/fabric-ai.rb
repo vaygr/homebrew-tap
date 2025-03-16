be63c3d5257fa088c966af245845f6e1d6a5bb17
Val V <scm@vaygr.net>
2025-03-16 20:42:15 +0000

    fabric-ai: => 1.4.158

--- Formula/fabric-ai.rb
+++ Formula/fabric-ai.rb
@@ -1,8 +1,8 @@
 class FabricAi < Formula
   desc "Open-source framework for augmenting humans using AI"
   homepage "https://danielmiessler.com/p/fabric-origin-story"
-  url "https://github.com/danielmiessler/fabric/archive/refs/tags/v1.4.156.tar.gz"
-  sha256 "1b774702dd4e4933411379a65ebd314c84e592b7265ab1ca08e846b8271739f3"
+  url "https://github.com/danielmiessler/fabric/archive/refs/tags/v1.4.158.tar.gz"
+  sha256 "d8baec4ed027c1809e16e92cda5a3f1fd699a5d607eab520e0422431823380ef"
   license "MIT"
   head "https://github.com/danielmiessler/fabric.git", branch: "main"
 
