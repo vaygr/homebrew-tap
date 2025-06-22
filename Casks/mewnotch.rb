91db50721410b0c6cc68b6f5667eb75f06ce9955
Val V <scm@vaygr.net>
2025-06-22 14:28:45 +0000

    mewnotch 1.0.0

--- Casks/mewnotch.rb
+++ Casks/mewnotch.rb
@@ -1,8 +1,8 @@
 cask "mewnotch" do
-  version "0.9"
-  sha256 "a2836e3658bab284c932aee0fbec067bc5ab163fedb48f5b6ba38a7a4ebfaaea"
+  version "1.0.0"
+  sha256 "e242b4e16fd47d7f9d08dec1ebb90ee4d9acafd845764f63271b2b1bb9ef8756"
 
-  url "https://github.com/monuk7735/mew-notch/releases/download/#{version}/MewNotch-Installer.dmg"
+  url "https://github.com/monuk7735/mew-notch/releases/download/#{version}/MewNotch-v#{version}.dmg"
   name "MewNotch"
   desc "MacBook notch enhancer app"
   homepage "https://github.com/monuk7735/mew-notch"
